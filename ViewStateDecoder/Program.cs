331d92c2daf1d61cdafac2a939147a06ebffc895
srinikonakanchi <srinivasa.kpv@gmail.com>
2017-12-04 20:03:51 -0500

    Created GUI for viewstate decoder

--- ViewStateDecoder/Program.cs
+++ ViewStateDecoder/Program.cs
@@ -14,7 +14,7 @@ namespace ViewStateDecoder
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Frm_ViewStateDecoder());
         }
     }
 }
